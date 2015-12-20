fe990af3ed532b6782deb1da8a320e6ba9381bac
joshcomley <joshcomley@googlemail.com>
2015-12-20 16:47:19 +0000

    Added blog post sample

---
+++ Spikes/Blog demo/AuthorisationServer/src/AuthorisationServer/Migrations/20151220150452_OpenIddict.cs
@@ -0,0 +1,32 @@
+using Microsoft.Data.Entity.Migrations;
+
+namespace AuthorisationServer.Migrations
+{
+    public partial class OpenIddict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Application",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    DisplayName = table.Column<string>(nullable: true),
+                    LogoutRedirectUri = table.Column<string>(nullable: true),
+                    RedirectUri = table.Column<string>(nullable: true),
+                    Secret = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Application", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Application");
+        }
+    }
+}
---
+++ Spikes/Blog demo/AuthorisationServer/src/AuthorisationServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using OpenIddict;
+
+namespace AuthorisationServer.Models
+{
+    public class ApplicationDbContext : OpenIddictContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        private static bool _databaseChecked;
+        // The following code creates the database and schema if they don't exist.
+        // This is a temporary workaround since deploying database through EF migrations is
+        // not yet supported in this release.
+        // Please see this http://go.microsoft.com/fwlink/?LinkID=615859 for more information on how to do deploy the database
+        // when publishing your application.
+        public void EnsureDatabaseCreated()
+        {
+            if (!_databaseChecked)
+            {
+                _databaseChecked = true;
+                this.Database.EnsureCreated();
+            }
+        }
+    }
+}
